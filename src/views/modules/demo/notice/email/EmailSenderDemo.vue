04bf61318067f6be5d78afbe053a381f95883f15
xxm1995 <xxm1995@outlook.com>
2023-02-20 14:40:46 +0800

    feat 邮件发送演示
    (cherry picked from commit ccc8a474a7cafacff1bdfd71c7b4c320f6617815)

---
+++ src/views/modules/demo/notice/email/EmailSenderDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="m-3 p-3 pt-3 bg-white">
+      <a-tabs default-active-key="1">
+        <a-tab-pane key="1" tab="简单邮件" force-render>
+          <simple-email-sender />
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="标准邮件" force-render>
+          <standard-email-sender />
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import SimpleEmailSender from './SimpleEmailSender.vue'
+  import StandardEmailSender from './StandardEmailSender.vue'
+</script>
+
+<style scoped></style>
---
+++ src/views/modules/demo/notice/email/StandardEmailSender.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>开发中</h1>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
