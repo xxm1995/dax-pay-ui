04678be4f4bcd98b914c825980beb467d46f07f3
xxm <xxm1995@outlook.com>
2023-05-22 22:28:00 +0800

    feat 商户操作项

--- src/views/modules/payment/merchant/MerchantInfoList.vue
+++ src/views/modules/payment/merchant/MerchantInfoList.vue
@@ -31,7 +31,7 @@
         </vxe-column>
         <vxe-column field="remark" title="商户备注" />
         <vxe-column field="createTime" title="创建时间" />
-        <vxe-column fixed="right" width="150" :showOverflow="false" title="操作">
+        <vxe-column fixed="right" width="220" :showOverflow="false" title="操作">
           <template #default="{ row }">
             <span>
               <a-link @click="show(row)">查看</a-link>
@@ -39,6 +39,10 @@
             <a-divider type="vertical" />
             <span>
               <a-link @click="edit(row)">编辑</a-link>
+            </span>
+            <a-divider type="vertical" />
+            <span>
+              <a-link @click="showApps(row)">应用列表</a-link>
             </span>
             <a-divider type="vertical" />
             <a-link danger @click="remove(row)">删除</a-link>
@@ -69,6 +73,7 @@
   import { FormEditType } from '/@/enums/formTypeEnum'
   import { useMessage } from '/@/hooks/web/useMessage'
   import { QueryField } from '/@/components/Bootx/Query/Query'
+  import { router } from '/@/router'
 
   // 使用hooks
   const { handleTableChange, pageQueryResHandel, resetQueryParams, pagination, pages, model, loading } = useTablePage(queryPage)
@@ -124,6 +129,10 @@
   // 查看
   function show(record) {
     merchantInfoEdit.init(record.id, FormEditType.Show)
+  }
+  // 查看
+  function showApps(record) {
+    router.push({})
   }
 
   // 删除
